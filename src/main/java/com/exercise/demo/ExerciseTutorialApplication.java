c7c8bb86cf3740e13cdbc08848c0e0aa1002507d
Saahil Shah <saahil18105@iiitd.ac.in>
2020-06-16 19:30:44 +0530

    First Commit

---
+++ src/main/java/com/exercise/demo/ExerciseTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseTutorialApplication.class, args);
+	}
+
+}
